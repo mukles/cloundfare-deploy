33cb77f24b400c23a0f7c0c137b6bedc46eef9d2
mukles <muklesdev@gmail.com>
2026-01-11 17:30:29 +0600

    feat: Add tag revalidation to GET endpoint for improved cache management

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,6 +1,7 @@
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
+  revalidateTag("my-tag"); // Revalidate all paths with the tag "my-tag"
   revalidatePath("/"); // Revalidate the homepage
 }
